375bf1793b56c45a9052afdd8a1cb9c8719ebf2a
Euryn <crossdeltavi@gmail.com>
2024-03-26 23:42:43 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App.tsx";
-
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
 import "@fontsource/fira-code/300.css";
 import "@fontsource/fira-code/400.css";
 import "@fontsource/fira-code/500.css";
 import "@fontsource/fira-code/700.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.tsx";
 
 ReactDOM.createRoot(
 	document.getElementById("root")!,
