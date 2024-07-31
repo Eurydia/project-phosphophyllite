887f5e9a0676f722f9876da942153388bd3c2146
Euryn <crossdeltavi@gmail.com>
2024-07-31 16:18:41 +0700

    improved logging messages

--- src-tauri/src/app/setup/paths.rs
+++ src-tauri/src/app/setup/paths.rs
@@ -1,6 +1,6 @@
 macro_rules! create_dir_if_needed {
-    ($dir_path:expr, $name:expr) => {{
-        log::trace!("Creating {} dir", $name);
+    ($dir_path:expr) => {{
+        log::trace!("Creating dir: {}", $dir_path.display());
         match std::fs::create_dir_all($dir_path) {
             Ok(_) => {
                 log::trace!("Ok");
@@ -14,10 +14,11 @@ macro_rules! create_dir_if_needed {
 }
 
 /// Create file if it does not exist
-/// Returns true if file already exists
+///
+/// Returns `Result<bool>` if file already exists
 macro_rules! create_file_if_needed {
-    ($file_path:expr, $name:expr) => {{
-        log::trace!("Checking if {} file already exists", $name);
+    ($file_path:expr) => {{
+        log::trace!("Checking if file exists: {}", $file_path.display());
         match $file_path.try_exists() {
             Ok(true) => {
                 log::trace!("Ok");
@@ -43,42 +44,36 @@ macro_rules! create_file_if_needed {
 
 fn create_database_file(handle: tauri::AppHandle) -> Result<(), &'static str> {
     let dir_path = crate::paths::get_database_dir(handle.clone())?;
-    create_dir_if_needed!(&dir_path, "database");
+    create_dir_if_needed!(&dir_path);
 
     let file_path = dir_path.join(crate::app::constants::DB_FILE_NAME);
-    create_file_if_needed!(file_path, "database");
+    let _ = create_file_if_needed!(file_path);
 
     Ok(())
 }
 
 fn create_secret_files(handle: tauri::AppHandle) -> Result<(), &'static str> {
     let dir_path = crate::paths::get_secret_dir(handle.clone())?;
-    create_dir_if_needed!(&dir_path, "secrets");
+    create_dir_if_needed!(&dir_path);
 
     let app_id_file_path = dir_path.join(crate::app::constants::APP_ID_FILE_NAME);
-    create_file_if_needed!(app_id_file_path, crate::app::constants::APP_ID_FILE_NAME);
+    create_file_if_needed!(app_id_file_path);
 
     let installation_id_file_path = dir_path.join(crate::app::constants::INSTALLATION_ID_FILE_NAME);
-    create_file_if_needed!(
-        installation_id_file_path,
-        crate::app::constants::INSTALLATION_ID_FILE_NAME
-    );
+    create_file_if_needed!(installation_id_file_path);
 
     let rsa_private_key_file_path = dir_path.join(crate::app::constants::RSA_PRIVATE_KEY_FILE_NAME);
-    create_file_if_needed!(
-        rsa_private_key_file_path,
-        crate::app::constants::RSA_PRIVATE_KEY_FILE_NAME
-    );
+    create_file_if_needed!(rsa_private_key_file_path);
 
     Ok(())
 }
 
 fn create_setting_files(handle: tauri::AppHandle) -> Result<(), &'static str> {
     let dir_path = crate::paths::get_setting_dir(handle.clone())?;
-    create_dir_if_needed!(&dir_path, "settings");
+    create_dir_if_needed!(&dir_path);
 
     let file_path = dir_path.join(crate::app::constants::SETTINGS_FILE_NAME);
-    match create_file_if_needed!(file_path, crate::app::constants::SETTINGS_FILE_NAME) {
+    match create_file_if_needed!(file_path) {
         Ok(true) => (),
         Ok(false) => {
             log::trace!("Populating initial settings");
@@ -93,12 +88,9 @@ fn create_setting_files(handle: tauri::AppHandle) -> Result<(), &'static str> {
 pub fn prepare_paths(handle: tauri::AppHandle) -> Result<(), &'static str> {
     log::trace!("Preparing paths");
 
-    create_dir_if_needed!(
-        crate::paths::get_migration_dir(handle.clone())?,
-        "migration"
-    );
-    create_dir_if_needed!(crate::paths::get_log_dir(handle.clone())?, "log");
-    create_dir_if_needed!(crate::paths::get_temp_dir(handle.clone())?, "temp");
+    create_dir_if_needed!(crate::paths::get_migration_dir(handle.clone())?);
+    create_dir_if_needed!(crate::paths::get_log_dir(handle.clone())?);
+    create_dir_if_needed!(crate::paths::get_temp_dir(handle.clone())?);
 
     create_database_file(handle.clone())?;
     create_secret_files(handle.clone())?;
