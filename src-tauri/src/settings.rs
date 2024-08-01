1b508fc6bd996629fbeeef8a005939cec33a3fd8
Euryn <crossdeltavi@gmail.com>
2024-08-01 13:01:11 +0700

    added documentation

--- src-tauri/src/settings.rs
+++ src-tauri/src/settings.rs
@@ -8,16 +8,13 @@ fn resolve_settings_file(handle: tauri::AppHandle) -> Result<std::path::PathBuf,
 
 /// Overrides the settings file with default settings.
 ///
-/// The function has three steps.
-/// 1. resolves the path to the settings file,
-/// 2. serializes the default settings to a JSON string,
-/// 3. writes the JSON string to the settings file.
+/// This function resolves the path to the settings file, serializes the default settings, and writes the serialized string to the settings file.
 ///
 /// # Error
 ///
-/// - when it cannot resolve the path to the settings file,
-/// - when `serde_json` cannot serialize the default settings,
-/// - when it cannot write the JSON string to the settings file.
+/// - Tauri cannot resolve the path to the settings file,
+/// - `serde_json` cannot serialize the default settings,
+/// - system cannot write the JSON string to the settings file.
 #[tauri::command]
 pub fn revert_app_settings(handle: tauri::AppHandle) -> Result<(), &'static str> {
     let path = resolve_settings_file(handle)?;
@@ -25,51 +22,46 @@ pub fn revert_app_settings(handle: tauri::AppHandle) -> Result<(), &'static str>
     let content = match serde_json::to_string_pretty(&crate::models::AppSettings::default()) {
         Ok(json_str) => json_str,
         Err(err) => {
-            log::error!(
-                "serde_json failed to serialize settings for write: \"{}\"",
-                err
-            );
-            return Err("Cannot serialize settings");
+            log::error!("serde_json cannot serialize settings: \"{}\"", err);
+            return Err("Cannot serialize setting");
         }
     };
 
     match std::fs::write(&path, content) {
         Ok(_) => Ok(()),
         Err(err) => {
-            log::error!(
-                "System cannot write serialized content to file: \"{}\"",
-                err
-            );
-            Err("Cannot write content")
+            log::error!("System cannot write settings to file: \"{}\"", err);
+            Err("Cannot write settings")
         }
     }
 }
 
+/// Reads and deserializes settings.
+///
+/// This function resolves the path to the settings file, reads the file content, and deserializes the content to `AppSettings` struct.
+///
+/// # Error
+///
+/// - Tauri cannot resolve the path to the settings file,
+/// - system cannot read file content,
+/// - `serde_json` cannot deserialize the file content.
 pub fn get_app_settings(
     handle: tauri::AppHandle,
 ) -> Result<crate::models::AppSettings, &'static str> {
-    let path = resolve_settings_file!();
+    let path = resolve_settings_file(handle)?;
 
-    log::trace!("Reading content");
     let content = match std::fs::read_to_string(&path) {
-        Ok(file_content) => {
-            log::trace!("Ok");
-            file_content
-        }
+        Ok(file_content) => file_content,
         Err(err) => {
-            log::error!("Cannot read content: \"{}\"", err);
-            return Err("Cannot read content");
+            log::error!("System cannot read settings: \"{}\"", err);
+            return Err("Cannot read settings");
         }
     };
 
-    log::trace!("Deserializing content");
     match serde_json::from_str(&content) {
-        Ok(app_settings) => {
-            log::trace!("Ok");
-            Ok(app_settings)
-        }
+        Ok(app_settings) => Ok(app_settings),
         Err(err) => {
-            log::error!("Cannot deserialize settings: \"{}\"", err);
+            log::error!("serde_json cannot deserialize settings: \"{}\"", err);
             Err("Cannot deserialize settings")
         }
     }
