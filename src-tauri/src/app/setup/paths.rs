d95c3266bad7d08530b9f5041b974545076527bd
Euryn <crossdeltavi@gmail.com>
2024-08-01 21:45:32 +0700

    added documentation and clean up log msg

--- src-tauri/src/app/setup/paths.rs
+++ src-tauri/src/app/setup/paths.rs
@@ -1,12 +1,13 @@
 macro_rules! create_dir_if_needed {
     ($dir_path:expr) => {{
-        log::trace!("Creating dir: \"{}\"", $dir_path.display());
         match std::fs::create_dir_all($dir_path) {
-            Ok(_) => {
-                log::trace!("Ok");
-            }
+            Ok(_) => (),
             Err(err) => {
-                log::error!("Cannot create dir: \"{}\"", err);
+                log::error!(
+                    "System cannot create dir at \"{}\": \"{}\"",
+                    $dir_path.display(),
+                    err
+                );
                 return Err("Cannot create dir");
             }
         }
@@ -14,48 +15,47 @@ macro_rules! create_dir_if_needed {
 }
 
 /// Create file if it does not exist
-///
-/// Returns `Result<bool>` if file already exists
 macro_rules! create_file_if_needed {
     ($file_path:expr) => {{
-        log::trace!("Checking if file exists: \"{}\"", $file_path.display());
         match $file_path.try_exists() {
-            Ok(true) => {
-                log::trace!("Ok");
-                Ok(true)
-            }
-            Ok(false) => {
-                log::trace!("File missing");
-                log::trace!("Creating file");
-                match std::fs::File::create($file_path) {
-                    Ok(_) => {
-                        log::trace!("Ok");
-                        Ok(false)
-                    }
-                    Err(err) => {
-                        log::error!("Cannot create file: \"{}\"", err);
-                        Err("Cannot create file")
-                    }
+            Ok(true) => Ok(true),
+            Ok(false) => match std::fs::File::create($file_path) {
+                Ok(_) => Ok(false),
+                Err(err) => {
+                    log::error!("System cannot create file: \"{}\"", err);
+                    Err("Cannot create file")
                 }
-            }
+            },
             Err(err) => {
-                log::error!("Cannot check if file exists: \"{}\"", err);
+                log::error!("System cannot check if file exists: \"{}\"", err);
                 Err("Cannot check if file exists")
             }
         }
     }};
 }
 
+/// Create directory and file for database.
+///
+/// Separately creates:
+/// - the directory for the database
+/// - the `.sqlite` database file
 fn create_database_file(handle: tauri::AppHandle) -> Result<(), &'static str> {
     let dir_path = crate::paths::get_database_dir(handle.clone())?;
     create_dir_if_needed!(&dir_path);
 
     let file_path = dir_path.join(crate::app::constants::DB_FILE_NAME);
-    let _ = create_file_if_needed!(file_path);
+    let _ = create_file_if_needed!(file_path)?;
 
     Ok(())
 }
 
+/// Create secret directory and secret files if they do not exist.
+///
+/// Separately creates:
+/// - the directory
+/// - the file which will store the app id
+/// - the file which will store the installation id
+/// - the file which will store the RSA private key
 fn create_secret_files(handle: tauri::AppHandle) -> Result<(), &'static str> {
     let dir_path = crate::paths::get_secret_dir(handle.clone())?;
     create_dir_if_needed!(&dir_path);
@@ -72,28 +72,31 @@ fn create_secret_files(handle: tauri::AppHandle) -> Result<(), &'static str> {
     Ok(())
 }
 
+/// Create setting directory and setting file if they do not exist.
+///
+/// Separately creates:
+/// - the directory for settings
+/// - the file which will store the settings
 fn create_setting_files(handle: tauri::AppHandle) -> Result<(), &'static str> {
     let dir_path = crate::paths::get_setting_dir(handle.clone())?;
     create_dir_if_needed!(&dir_path);
 
     let file_path = dir_path.join(crate::app::constants::SETTINGS_FILE_NAME);
     match create_file_if_needed!(file_path) {
-        Ok(true) => {
-            log::trace!("Ok");
-            Ok(())
-        }
-        Ok(false) => {
-            log::trace!("Writing initial settings");
-            crate::settings::revert_app_settings(handle)?;
-            Ok(())
-        }
+        Ok(true) => Ok(()),
+        Ok(false) => crate::settings::revert_app_settings(handle),
         Err(err) => Err(err),
     }
 }
 
+/// Prepare all paths and files needed by the application.
+///
+/// Separately creates:
+/// - directories for migrations, logs, and temporary files
+/// - the database file
+/// - the secret files
+/// - the setting files
 pub fn prepare_paths(handle: tauri::AppHandle) -> Result<(), &'static str> {
-    log::trace!("Preparing paths");
-
     create_dir_if_needed!(crate::paths::get_migration_dir(handle.clone())?);
     create_dir_if_needed!(crate::paths::get_log_dir(handle.clone())?);
     create_dir_if_needed!(crate::paths::get_temp_dir(handle.clone())?);
