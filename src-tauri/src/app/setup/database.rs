4922b67c24fbd218bb10a627e0326da5eace0fa1
Euryn <crossdeltavi@gmail.com>
2024-08-01 21:50:32 +0700

    cleaned up log msg

--- src-tauri/src/app/setup/database.rs
+++ src-tauri/src/app/setup/database.rs
@@ -3,31 +3,22 @@ async fn prepare_db_connection_pool(
     handle: tauri::AppHandle,
 ) -> Result<sqlx::Pool<sqlx::Sqlite>, &'static str> {
     {
-        log::trace!("Preparing path to database");
         let dir_path = crate::paths::get_database_dir(handle.clone())?;
         let file_path = dir_path.join(crate::app::constants::DB_FILE_NAME);
 
-        log::trace!("Converting path to string");
         let file_path_str = match file_path.to_str() {
-            Some(path) => {
-                log::trace!("Ok");
-                path
-            }
+            Some(path) => path,
             None => {
-                log::error!("Cannot convert path to string");
+                log::error!("Rust cannot convert path to string");
                 return Err("Cannot convert path to string");
             }
         };
 
-        log::trace!("Connecting to database");
         let pool = sqlx::sqlite::SqlitePoolOptions::new().connect(file_path_str);
         match pool.await {
-            Ok(conn) => {
-                log::trace!("Ok");
-                Ok(conn)
-            }
+            Ok(conn) => Ok(conn),
             Err(err) => {
-                log::error!("Cannot connect to database: \"{}\"", err);
+                log::error!("sqlx cannot connect to database: \"{}\"", err);
                 Err("Cannot connect to database")
             }
         }
@@ -35,37 +26,26 @@ async fn prepare_db_connection_pool(
 }
 
 /// Prepare database and run migrations
+///
+/// This function prepares the database connection pool and runs the migrations.
 pub async fn prepare_db(app: &tauri::App) -> Result<sqlx::pool::Pool<sqlx::Sqlite>, &'static str> {
-    log::trace!("Preparing database connection");
     let db = prepare_db_connection_pool(app.handle()).await?;
 
     let migrations_dir = crate::paths::get_migration_dir(app.handle())?;
 
-    log::trace!(
-        "Building migrator from dir: \"{}\"",
-        migrations_dir.display()
-    );
     let migrator = match sqlx::migrate::Migrator::new(migrations_dir).await {
-        Ok(migrator) => {
-            log::trace!("Ok");
-            migrator
-        }
+        Ok(migrator) => migrator,
         Err(err) => {
-            log::error!("Cannot build migrator: \"{}\"", err);
+            log::error!("sqlx cannot build migrator: \"{}\"", err);
             return Err("Cannot build migrator");
         }
     };
 
-    log::trace!("Running migrator");
     match migrator.run(&db).await {
-        Ok(_) => {
-            log::trace!("Ok");
-        }
+        Ok(_) => Ok(()),
         Err(err) => {
-            log::error!("Cannot run migrator: \"{}\"", err);
-            return Err("Cannot run migrator");
+            log::error!("sqlx cannot run migrator: \"{}\"", err);
+            Err("Cannot run migrator")
         }
     }
-
-    Ok(db)
 }
